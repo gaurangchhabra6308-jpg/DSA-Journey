4c4b02e59bfdc0711bbc39f5f1b0ad8a6caac17f
GAURANG CHHABRA <gaurangchhabra6308@gmail.com>
2026-07-26 00:58:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (89.66%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int max1=0,max2=0;
+        while(n!=0){
+            int dig=n%10;
+            if(max1<=dig){
+                max2=max1;
+                max1=dig;
+            }
+            else if(max2<dig){
+                max2=dig;
+            }
+            n=n/10;
+
+        }
+        return max1*max2;
+    }
+};
