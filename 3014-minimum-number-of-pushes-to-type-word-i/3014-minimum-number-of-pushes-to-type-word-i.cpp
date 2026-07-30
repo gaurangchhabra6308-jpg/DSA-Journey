a78de372a08ae8f575e69853817e28ae1f3b33d6
GAURANG CHHABRA <gaurangchhabra6308@gmail.com>
2026-07-30 17:52:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n=word.length();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            cnt+=i/8+1;
-        }
-        return cnt;
+     int n=word.size();
+     int block=n/8;
+     int rem=n%8;
+     int cnt= (block*(block+1)*4)+rem*(block+1);
+     return cnt;
     }
 };
