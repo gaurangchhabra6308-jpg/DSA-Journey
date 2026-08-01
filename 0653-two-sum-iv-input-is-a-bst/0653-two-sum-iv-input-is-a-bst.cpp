a72308b4f7a2cf0974c566aa3568b0dd1a0bff4e
GAURANG CHHABRA <gaurangchhabra6308@gmail.com>
2026-08-01 12:25:31 +0530

    Time: 0 ms (100%), Space: 37.3 MB (64.23%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,12 +11,75 @@
  */
 class Solution {
 public:
-      unordered_set<int>set;
-    bool findTarget(TreeNode* root, int k) {
+      class BSTIterator {
+    stack<TreeNode*> st;
+
+    void pushLeft(TreeNode* node){
+       while(node){
+        st.push(node);
+        node=node->left;
+       }
+    }
+
+public:
+    BSTIterator(TreeNode* root){
+        pushLeft(root);
+    }
+
+    int next(){
+            TreeNode* node = st.top();
+    st.pop();
+
+    if(node->right)
+        pushLeft(node->right);
+
+    return node->val;
+
+    }
+};
+    class BSTReverseIterator {
+    stack<TreeNode*> st;
+
+    void pushRight(TreeNode* node){
+        while (node) {
+    st.push(node);
+    node = node->right;
+}
+    }
+
+public:
+    BSTReverseIterator(TreeNode* root){
+        pushRight(root);
+    }
+
+    int next(){
        
-        if(!root) return false;
-        if(set.count(k-root->val)) return true;
-        set.insert(root->val);
-        return findTarget(root->left,k)|| findTarget(root->right,k);
+    TreeNode* node = st.top();
+    st.pop();
+
+    if(node->left)
+        pushRight(node->left);
+
+    return node->val;
+
+    }
+};
+    bool findTarget(TreeNode* root, int k) {
+     BSTIterator leftIt(root);
+     BSTReverseIterator rightIt(root);
+    int left = leftIt.next();
+    int right = rightIt.next();
+    while(left < right){
+
+    if(left + right == k)
+        return true;
+
+    else if(left + right < k)
+        left = leftIt.next();
+
+    else
+        right = rightIt.next();
+}
+return false;
     }
 };
